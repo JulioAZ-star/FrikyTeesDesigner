99bdda6594540bfc7948217f495f1c4f520bca92
JulioAZ-star <dioss666@gmail.com>
2026-07-11 11:13:29 +0200

    Base del personalizador
    Primera estructura del proyecto con Konva y carga de imágenes

---
+++ js/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    canvas: {
+        width: 700,
+        height: 800
+    },
+    printArea: {
+        x: 150,
+        y: 100,
+        width: 400,
+        height: 500
+    }
+};
